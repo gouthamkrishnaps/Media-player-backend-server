7f3f23296f6f5331394ded9ec850a9c9826851f7
gouthamkrishnaps <gouthamkrishnaps02@gmail.com>
2023-11-09 10:55:26 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+//import json server library in index.js file 
+const jsonServer = require('json-server')
+
+//create json server
+const mediaPlayerServer =  jsonServer.create()
+
+//create path to json file
+const router = jsonServer.router('database.json')
+
+//middleware to convert json to js
+const middleware = jsonServer.defaults()
+
+//use middleware and router in server
+mediaPlayerServer.use(middleware)
+mediaPlayerServer.use(router)
+
+//setup port for running server
+const port = 5000 || process.env.PORT
+
+//to listen server for resolving request request from client 
+mediaPlayerServer.listen(port,()=>{
+    console.log(`mediaplayerserver started at ${port} and waiting for request`);
+})
